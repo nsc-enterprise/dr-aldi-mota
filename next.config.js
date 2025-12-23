652c0dc6a2762445e54c1973550e643215b12bfe
lioncarballo88 <lioncarballo@gmail.com>
2025-12-22 20:56:15 -0600

    fixed ts  code

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: false,
+  experimental: {
+    esmExternals: false
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      }
+    }
+    return config
+  },
+  output: 'standalone'
 }
 
 module.exports = nextConfig
